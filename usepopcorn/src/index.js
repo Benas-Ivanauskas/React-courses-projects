fb1227da0e0539f0e626a3cf55fbd8c9613f360a
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2024-02-09 11:02:07 +0200

    updating full project

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <App />
+    {/*<StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      defaultRating={1}
+    />
+    <StarRating color="red" className="test" />
+<StarRating />*/}
   </React.StrictMode>
 );
